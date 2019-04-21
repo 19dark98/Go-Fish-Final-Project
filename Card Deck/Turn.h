48266500f49f4cea2eaf69322b95264f8ea97669
Jackson Dorsett <dorsettj@gmail.com>
2019-04-21 13:39:49 -0700

    fixes

--- Card Deck/Turn.h
+++ Card Deck/Turn.h
@@ -15,20 +15,23 @@ private:
 	sf::Texture BackgroundTexture;
 	sf::Sprite BackgroundSprite;
 
-	void drawBackground();
+	void drawBackground();//done needs testing
+	
+	//Needs Coding
 	void drawPlayerHand();
 	void drawCpuHand();
 	void drawDeck();
 	void drawPairNum();
 
 	//functions called on initialization
-	void loadBackground();
-	void resizeBackground();
+	void loadBackground();//needs testing
+	void resizeBackground();//needs testing
 
 protected:
 	bool isGameOver();
+	void resetGame();
 
 	//virtual functions that will be overridden in derived class
-	virtual void chooseCard() = 0;
+	//virtual void chooseCard() = 0;
 };
 
